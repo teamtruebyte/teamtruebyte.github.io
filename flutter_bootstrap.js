c068f42bf16d12c5365ce476d66aed22d19cebfc
teamtruebyte <teamtruebyteindia@gmail.com>
2026-07-23 23:57:48 +0530

    add search bar

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "626650816" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "949595004" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
