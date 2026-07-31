bfd145c306198b75b21f7a92240f852342ca0987
teamtruebyte <teamtruebyteindia@gmail.com>
2026-07-31 08:44:34 +0530

    workflow change

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2453294210" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2839158197" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
